3cc19baaa96483513d377385257d3fac9625a905
Devin Raleigh <devinkr@gmail.com>
2022-12-02 14:19:04 -0700

    basic typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// let name: string;
+// let age: number | string;
+
+// let student: boolean;
+// let hobbies: string[];
+// let role:[number, string]
+
+// let printName: (name:string) => never;
+
+// let personName: unknown;
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "Devin",
+//   age: 39
+// }
+
+// let lotsOfPeople: Person[];
+
+// interface Person {
+//   name: string;
+//   age?: number;
+// }
+
+// interface Guy extends Person {
+//   profession: string;
+// }
+
+// type X = {
+//   a: string;
+//   b: number;
+// }
+
+// & is the same as extends with interface. 
+// type Y = X & {
+//   c: string;
+//   d: number;
+// }
+
+// let y:Y = {
+//   c: 'efwef',
+//   d: 34
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Hello World</div>
   );
 }
 
